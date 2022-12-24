06f4359ccb796284fc333880762f749e40c4696d
volodymyr.petrovkyi <volodymyr.petrovkyi@gmail.com>
2022-12-24 12:56:33 +0200

    Initial commit

---
+++ src/main/java/com/example/testTask/TestTask1Application.java
@@ -0,0 +1,13 @@
+package com.example.testTask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestTask1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestTask1Application.class, args);
+    }
+
+}
