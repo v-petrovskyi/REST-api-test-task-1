68ff9d27e5195c308f45638e2431a2ac8fe982d5
Volodymyr <volodymyr.petrovskyi@gmail.com>
2022-12-24 13:28:54 +0200

    create PriceRepository

---
+++ src/main/java/com/example/testTask/repositories/PriceRepository.java
@@ -0,0 +1,11 @@
+package com.example.testTask.repositories;
+
+import com.example.testTask.entity.Price;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface PriceRepository extends MongoRepository<Price, String> {
+
+    public Price getPriceBySymbol1AndSymbol2OrderByLpriceAsc(String symbol1, String symbol2);
+    public Price getPriceBySymbol1AndSymbol2OrderByLpriceDesc(String symbol1, String symbol2);
+
+}
