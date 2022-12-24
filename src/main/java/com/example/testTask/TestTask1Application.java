d98a9cba2167bfbe3d1a4187f02161942155ac98
Volodymyr <volodymyr.petrovskyi@gmail.com>
2022-12-24 13:29:23 +0200

    change TestTask1Application

--- src/main/java/com/example/testTask/TestTask1Application.java
+++ src/main/java/com/example/testTask/TestTask1Application.java
@@ -1,13 +1,26 @@
 package com.example.testTask;
 
+import com.example.testTask.repositories.PriceRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.time.LocalDateTime;
+
 @SpringBootApplication
-public class TestTask1Application {
+public class TestTask1Application implements CommandLineRunner {
+
+    @Autowired
+    private PriceRepository repository;
 
     public static void main(String[] args) {
         SpringApplication.run(TestTask1Application.class, args);
+    }
+
+    public void run(String... args){
+        repository.save("BTC", "USD", 1535.8545, System.currentTimeMillis());
+
     }
 
 }
