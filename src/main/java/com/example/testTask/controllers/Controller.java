9b7432a9b8316fade9bc048685d6fe12c95aa3a4
Volodymyr <volodymyr.petrovskyi@gmail.com>
2022-12-24 15:51:27 +0200

    v0.4 fix Controller.java (works correct)

--- src/main/java/com/example/testTask/controllers/Controller.java
+++ src/main/java/com/example/testTask/controllers/Controller.java
@@ -3,12 +3,14 @@ package com.example.testTask.controllers;
 import com.example.testTask.entity.Price;
 import com.example.testTask.services.PriceService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@RestController("/cryptocurrencies")
+@RestController
+@RequestMapping("/cryptocurrencies")
 public class Controller {
 
     private final PriceService priceService;
@@ -20,6 +22,11 @@ public class Controller {
     @GetMapping("/minprice")
     public Price returnMinPrice(@RequestParam("name") String currencyName) {
         return priceService.getMin(currencyName);
+    }
+
+    @GetMapping("/maxprice")
+    public Price returnManPrice(@RequestParam("name") String currencyName) {
+        return priceService.getMax(currencyName);
     }
 
     @GetMapping("/all")
