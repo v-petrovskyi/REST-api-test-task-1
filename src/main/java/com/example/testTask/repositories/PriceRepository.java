9280e4ab6bc32f22272a71980485e5b186ed67a7
Volodymyr <volodymyr.petrovskyi@gmail.com>
2022-12-25 12:30:36 +0200

    v0.12 create method getPriceByCurr1AndCurr2OrderByLpriceAsc in PriceRepository.java

--- src/main/java/com/example/testTask/repositories/PriceRepository.java
+++ src/main/java/com/example/testTask/repositories/PriceRepository.java
@@ -3,9 +3,11 @@ package com.example.testTask.repositories;
 import com.example.testTask.entity.Price;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.List;
+
 public interface PriceRepository extends MongoRepository<Price, String> {
      Price getFirstByCurr1AndCurr2OrderByLpriceAsc(String curr1, String curr2);
-
      Price getFirstByCurr1AndCurr2OrderByLpriceDesc(String curr1, String curr2);
+     List<Price> getPriceByCurr1AndCurr2OrderByLpriceAsc(String curr1, String curr2);
 
 }
