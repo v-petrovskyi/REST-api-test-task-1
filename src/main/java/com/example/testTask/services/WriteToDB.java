93c1d2bab7d6936201ee181216e96fef59d45072
volodymyr.petrovkyi <volodymyr.petrovkyi@gmail.com>
2022-12-25 10:46:09 +0200

    v0.8.1

--- src/main/java/com/example/testTask/services/WriteToDB.java
+++ src/main/java/com/example/testTask/services/WriteToDB.java
@@ -6,8 +6,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @Service
 public class WriteToDB {
+    private final Map<String, String> lastPricesMap = new HashMap<>();
 
     private final PriceService priceService;
 
@@ -15,14 +19,28 @@ public class WriteToDB {
         this.priceService = priceService;
     }
 
-    public void write(String curr1, String curr2){
-        PairPrice pairPriceBTC = new CexIoJson(new RestTemplate()).getLastPrice(curr1, curr2);
-        try{
-            priceService.add(PairPriceToPrice.convertToPrice(pairPriceBTC));
-        } catch (NullPointerException e){
+    public void write(String curr1, String curr2) {
+        PairPrice pairPrice = new CexIoJson(new RestTemplate()).getLastPrice(curr1, curr2);
+        System.out.println(pairPrice);
+        String key = curr1 + "/" + curr2;
+        if (!lastPricesMap.containsKey(key)) {
+            lastPricesMap.put(key, pairPrice.getLprice());
+            try {
+                priceService.add(PairPriceToPrice.convertToPrice(pairPrice));
+            } catch (NullPointerException e) {
+                e.printStackTrace();
+                System.out.println(pairPrice);
+            }
+        }
+        if (lastPricesMap.get(key).equals(pairPrice.getLprice())){
+            return;
+        }
+        lastPricesMap.put(key, pairPrice.getLprice());
+        try {
+            priceService.add(PairPriceToPrice.convertToPrice(pairPrice));
+        } catch (NullPointerException e) {
             e.printStackTrace();
         }
-
     }
 
 }
