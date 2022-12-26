54177626becaadc2cf59a6a0fad494b3571969c1
volodymyr.petrovkyi <volodymyr.petrovkyi@gmail.com>
2022-12-26 17:41:28 +0200

    v0.39 created IncorrectPageException class

---
+++ src/main/java/com/example/testTask/exeptions/IncorrectPageException.java
@@ -0,0 +1,7 @@
+package com.example.testTask.exeptions;
+
+public class IncorrectPageException extends Exception{
+    public IncorrectPageException(String message) {
+        super(message);
+    }
+}
