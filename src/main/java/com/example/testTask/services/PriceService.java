2a6be2d45e6375b27e66512c7b9627ff48e693e2
volodymyr.petrovkyi <volodymyr.petrovkyi@gmail.com>
2022-12-26 17:42:21 +0200

    v0.40 method getSelectedPageOfCurrencies can throw IncorrectPageException

--- src/main/java/com/example/testTask/services/PriceService.java
+++ src/main/java/com/example/testTask/services/PriceService.java
@@ -3,6 +3,7 @@ package com.example.testTask.services;
 
 import com.example.testTask.entity.Price;
 import com.example.testTask.exeptions.CurrencyException;
+import com.example.testTask.exeptions.IncorrectPageException;
 
 import java.util.List;
 import java.util.Map;
@@ -12,7 +13,7 @@ public interface PriceService {
     Price getMin(String currency) throws CurrencyException;
     Price getMax(String currency) throws CurrencyException;
     List<Price> getAll();
-    List<Price> getSelectedPageOfCurrencies(String currency, int page, int size) throws CurrencyException;
+    List<Price> getSelectedPageOfCurrencies(String currency, int page, int size) throws CurrencyException, IncorrectPageException;
     List<String> listForCSVReport() throws CurrencyException;
 }
 
--- src/main/java/com/example/testTask/services/impl/PriceServiceImpl.java
+++ src/main/java/com/example/testTask/services/impl/PriceServiceImpl.java
@@ -2,6 +2,7 @@ package com.example.testTask.services.impl;
 
 import com.example.testTask.entity.Price;
 import com.example.testTask.exeptions.CurrencyException;
+import com.example.testTask.exeptions.IncorrectPageException;
 import com.example.testTask.repositories.PriceRepository;
 import com.example.testTask.services.PriceService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -50,13 +51,17 @@ public class PriceServiceImpl implements PriceService {
     }
 
     @Override
-    public List<Price> getSelectedPageOfCurrencies(String currency, int page, int size) throws CurrencyException {
+    public List<Price> getSelectedPageOfCurrencies(String currency, int page, int size) throws CurrencyException, IncorrectPageException {
         if (currency.equals("BTC/USD") || currency.equals("ETH/USD") || currency.equals("XRP/USD")) {
             String[] split = currency.split("/");
             List<Price> priceList = repository.getPriceByCurr1AndCurr2OrderByLpriceAsc(split[0], split[1]);
             int fromIndex = (page * size) - size;
             int toIndex = fromIndex + size;
-            return priceList.subList(fromIndex, toIndex);
+            try {
+                return priceList.subList(fromIndex, toIndex);
+            } catch (IndexOutOfBoundsException e) {
+                throw new IncorrectPageException("page " + page + " not exist");
+            }
         } else {
             throw new CurrencyException("currency " + currency + " not exist");
         }
